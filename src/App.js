5c1cddbdb1119009fdcf4a947861df5ade8c44cc
Anaïs Rouvière <anais.rouviere@kobalt.fr>
2020-11-09 00:52:14 +0100

    Utilisation de prop-types

--- src/App.js
+++ src/App.js
@@ -4,7 +4,21 @@ import Contact from "./components/Contact";
 function App() {
   return (
     <div className="App">
-        <Contact/>
+        <Contact
+            name="Guts"
+            image="https://images6.fanpop.com/image/photos/42700000/Guts-berserk-the-anime-manga-42723571-2048-2048.jpg"
+            online
+        />
+        <Contact
+            name="Casca"
+            image="https://i.ytimg.com/vi/nkFCnaYSBDE/hqdefault.jpg"
+            online={false}
+        />
+        <Contact
+            name="Griffith"
+            image="https://i.pinimg.com/originals/ce/11/6e/ce116e263aa25fae791f02213c82b7f3.jpg"
+            online={false}
+        />
     </div>
   );
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,34 +1,26 @@
 import React from "react";
 import "./Contact.css";
+import PropTypes from 'prop-types';
 
-const onlineGuts = true;
-const onlineCasca = false;
-
-function Contact() {
+function Contact(props) {
     return (
         <div>
             <div className="Contact">
-                <img className="avatar" src="https://images6.fanpop.com/image/photos/42700000/Guts-berserk-the-anime-manga-42723571-2048-2048.jpg"/>
+                <img className="avatar" src={props.image}/>
                 <div>
-                    <p className="name">Guts</p>
+                    <p className="name">{props.name}</p>
                     <div className="status">
-                        <span className={onlineGuts ? "status-online" : "status-offline"}/>
-                        <p className="status-text">{onlineGuts ? "online" : "offline"}</p>
-                    </div>
-                </div>
-            </div>
-            <div className="Contact">
-                <img className="avatar" src="https://i.ytimg.com/vi/nkFCnaYSBDE/hqdefault.jpg"/>
-                <div>
-                    <p className="name">Casca</p>
-                    <div className="status">
-                        <span className={onlineCasca ? "status-online" : "status-offline"}/>
-                        <p className="status-text">{onlineCasca ? "online" : "offline"}</p>
+                        <span className={props.online ? "status-online" : "status-offline"}/>
+                        <p className="status-text">{props.online ? "online" : "offline"}</p>
                     </div>
                 </div>
             </div>
         </div>
     );
 }
-
+Contact.propTypes = {
+    name: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    online: PropTypes.bool.isRequired
+};
 export default Contact;
