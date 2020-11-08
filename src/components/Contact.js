f739314c8dae87ffce520fa89a883c2eb068a6ac
Anaïs Rouvière <anais.rouviere@kobalt.fr>
2020-11-09 00:08:34 +0100

    Modifs sur le status

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,15 +1,30 @@
 import React from "react";
 import "./Contact.css";
 
+const onlineGuts = true;
+const onlineCasca = false;
+
 function Contact() {
     return (
-        <div className="Contact">
-            <img className="avatar" src="https://randomuser.me/api/portraits/men/78.jpg"/>
-            <div>
-                <p className="name">Charles Ingalls</p>
-                <div className="status">
-                    <span className="status-online"/>
-                    <p className="status-text">online</p>
+        <div>
+            <div className="Contact">
+                <img className="avatar" src="https://images6.fanpop.com/image/photos/42700000/Guts-berserk-the-anime-manga-42723571-2048-2048.jpg"/>
+                <div>
+                    <p className="name">Guts</p>
+                    <div className="status">
+                        <span className={onlineGuts ? "status-online" : "status-offline"}/>
+                        <p className="status-text">{onlineGuts ? "online" : "offline"}</p>
+                    </div>
+                </div>
+            </div>
+            <div className="Contact">
+                <img className="avatar" src="https://i.ytimg.com/vi/nkFCnaYSBDE/hqdefault.jpg"/>
+                <div>
+                    <p className="name">Casca</p>
+                    <div className="status">
+                        <span className={onlineCasca ? "status-online" : "status-offline"}/>
+                        <p className="status-text">{onlineCasca ? "online" : "offline"}</p>
+                    </div>
                 </div>
             </div>
         </div>
